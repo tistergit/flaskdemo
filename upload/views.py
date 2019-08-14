e0a5064de2b240e70e5c3fda1b81ae10cc81a6a2
tisteryu <tisteryu@tencent.com>
2019-08-14 17:35:43 +0800

    区分文件上传和发布

--- upload/views.py
+++ upload/views.py
@@ -3,7 +3,7 @@
 # Created by Flynn on 2018-08-29 18:12
 
 
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template,jsonify
 from flask_uploads import UploadSet, IMAGES,DEFAULTS,ARCHIVES
 import logging
 from flask import current_app
@@ -12,6 +12,7 @@ import time
 import hashlib
 import os
 from pathlib import Path
+import zipfile,shutil
 
 
 bp = Blueprint('upload', __name__, url_prefix='/upload2/')
@@ -38,30 +39,33 @@ def file_upload():
         current_app.logger.info("name : " + name)
         filename = photos.save(request.files['file'], name=name+'.')
 
-        import zipfile,shutil
-        p = Path("uploads/tmp/")
-        p.mkdir(exist_ok=True)
-        ###
-        with zipfile.ZipFile(request.files['file'], 'r') as zf:
-            for fn in zf.namelist():
-                dest_fn = "uploads/tmp/" + fn
-                # current_app.logger.info("dest_fn : " + dest_fn)
-                right_fn = dest_fn.encode('cp437').decode('utf8')  # 将文件名正确编码
-                if right_fn.endswith("/"):
-                    if not os.path.exists(right_fn):
-                        os.mkdir(right_fn)
-                else:
-                    with open(right_fn, 'wb') as output_file:  # 创建并打开新文件
-                        with zf.open(fn, 'r') as origin_file:  # 打开原文件
-                            shutil.copyfileobj(origin_file, output_file)  # 将原文件内容复制到新文件
-                            
-                
+    return jsonify({'filename':filename}), 200
 
+@bp.route('/mkdocs', methods=['POST', 'GET'])
+def mkdocs():
+    current_app.logger.info("values : " + str(request.json))
+    p = Path("uploads/tmp/")
+    p.mkdir(exist_ok=True)
+    ###
+    # with zipfile.ZipFile(request.files['file'], 'r') as zf:
+    #     for fn in zf.namelist():
+    #         dest_fn = "uploads/tmp/" + fn
+    #         # current_app.logger.info("dest_fn : " + dest_fn)
+    #         right_fn = dest_fn.encode('cp437').decode('utf8')  # 将文件名正确编码
+    #         if right_fn.endswith("/"):
+    #             if not os.path.exists(right_fn):
+    #                 os.mkdir(right_fn)
+    #         else:
+    #             with open(right_fn, 'wb') as output_file:  # 创建并打开新文件
+    #                 with zf.open(fn, 'r') as origin_file:  # 打开原文件
+    #                     shutil.copyfileobj(origin_file, output_file)  # 将原文件内容复制到新文件
+                        
+            
+    return jsonify({'filename':'filename'}), 200 
 
-        # r = os.system("cd uploads/tmp;mkdocs build -d " + doc_version)
-        # if r != 0:
-        #     return "file upload error!!!",500
-    return "file upload success" , 200
+    # r = os.system("cd uploads/tmp;mkdocs build -d " + doc_version)
+    # if r != 0:
+    #     return "file upload error!!!",500
 
 @bp.route('/', methods=['POST', 'GET'])
 def index():
